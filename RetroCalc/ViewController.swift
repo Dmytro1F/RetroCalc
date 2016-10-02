e53279693d93044277d460c3750855bb1d13ed30
Dmytro <dmytro.fedorov@icloud.com>
2016-10-02 13:55:12 +0400

    Added code for the math operations
    +Fixed the buttons, the Calc working nicely. Now the time to upgrade
    it…!!!

--- RetroCalc/ViewController.swift
+++ RetroCalc/ViewController.swift
@@ -10,13 +10,46 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
 
+    @IBOutlet weak var outputLbl: UILabel!
     
     @IBAction func numberPressed(sender: UIButton) { //Buttons Connected to play Sound
         
         playSound()
         
+        runningNumber += "\(sender.tag)" //making btn tags work as a magick
+        outputLbl.text = runningNumber
     }
+    
+    @IBAction func onDividePressed (sender: AnyObject) {
+        
+        processOperation(operation : .Divide)
+        
+    }
+    @IBAction func onMultiplyPressed (sender: AnyObject) {
+        
+        processOperation(operation : .Multiply)
+        
+    }
+    @IBAction func onSubtractPressed (sender: AnyObject) {
+        
+        processOperation(operation : .Subtract)
+        
+    }
+    @IBAction func onAddPressed (sender: AnyObject) {
+        
+        processOperation(operation : .Add)
+        
+    }
+    
+    @IBAction func onEauqlPressed (sender: AnyObject) {
+        
+        processOperation(operation: currentOperation)
+        
+    }
+    
     
     func playSound() { // Function to play Sound
         
@@ -28,10 +61,68 @@ class ViewController: UIViewController {
     }
     
     var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
     
+    var currentOperation = Operation.Empty
+    
+    
+    enum Operation: String { //TODO: Need to understand
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            
+            //A user selected operator, and then selected another operator without first entering number
+            
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply{
+                    
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                    
+                } else if currentOperation == Operation.Divide{
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract{
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add{
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = operation
+        }
+        else {
+            
+            // this is the first time operator being pressed
+            
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+            
+        }
+    }
+
     override func viewDidLoad() { // Making code for the Sound!
         
         super.viewDidLoad()
+        
+         outputLbl.text = "0"
+        
         
         let path = Bundle.main.path(forResource: "btn", ofType: "wav") //Finding the path of our audio file
         let soundURL = URL(fileURLWithPath: path!) // Declaring that path (URL)
@@ -46,6 +137,9 @@ class ViewController: UIViewController {
             
         }
         
+        
+        
  
 
-    }}
+    }
+}
